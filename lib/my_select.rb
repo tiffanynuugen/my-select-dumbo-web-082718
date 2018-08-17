6ff68c125388a957c41ceaf059d4e9682f4d724b
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-17 18:29:16 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- collection.select do |num| 
+def my_select(array)
+  i = 0
+  new_array = []
+  while i < array.size
+    if yield(array[i]) == true
+      new_array << array[i]
+    else
+      #nothing
+    end
+    i += 1
+  end
+  new_array
 end
